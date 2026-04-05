ea3d4ece39550730e911367bc5d3e24ce328b865
Naveenmayur <naveenmayurkr@gmail.com>
2026-04-05 14:55:40 +0530

    init: initial spring boot project structure and setup

---
+++ src/main/java/com/example/springbootwikimedia/SpringbootWikimediaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWikimediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWikimediaApplication.class, args);
+    }
+
+}
